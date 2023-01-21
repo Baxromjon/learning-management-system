9962fe9ec0f078db8b96a4a0254d2ca948dc6515
Baxromjon <73574634+Baxromjon@users.noreply.github.com>
2023-01-21 12:54:57 +0500

    edit home page

--- lms-react/src/pages/Home.js
+++ lms-react/src/pages/Home.js
@@ -4,7 +4,12 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Home page</h1>
+                <h1 className="text-center">LMS(Learning Management System) Home page</h1>
+                <div style={{marginLeft:"550px", marginRight:"auto"}}>
+                    <button className="btn btn-success m-2" style={{backgroundColor:"#029c14"}}><a href="/sign-up" style={{color:"#fff"}}>Sign_UP</a></button>
+                    <button className="btn btn-info m-2" style={{backgroundColor:"#0330fc"}}><a href="/sign-in" style={{color:"#fff"}}>Sign_IN</a></button>
+                </div>
+
             </div>
         );
     }
